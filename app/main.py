63e63e9af978d6716ee2b7ed6e8aad82c59633b4
ghamzak <gh.kazeminejad@gmail.com>
2025-07-14 07:42:17 -0700

    updated port reading on main + gitignore for untracking static and pycache

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from app import create_app
+import argparse
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    parser = argparse.ArgumentParser(description="Run the RAG PDF Chatbot Flask app.")
+    parser.add_argument('--port', type=int, default=5000, help='Port to run the server on (default: 5000)')
+    args = parser.parse_args()
+    app.run(debug=True, host='0.0.0.0', port=args.port)
